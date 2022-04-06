f0cbc85977f0af6c0c5f048929b13d856affaf51
maticamisay <maticamisay99@hotmail.com>
2022-04-05 23:45:25 -0300

    agregando estilos globales con gatsby-plugin-emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,53 @@
 import React from "react"
-import Header from "./header"
+import { Global, css } from "@emotion/react"
 import Helmet from "react-helmet"
+import Header from "./header"
 
 const Layout = ({ children }) => {
-  return (
-    <>
-      <Helmet>
-        <title>Gatsby Hotel</title>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
-      <Header />
+  return (<>
+    <Global styles={css`
+          html {
+            font-size: 62.5%;
+          }
+          body {
+            font-size: 16px;
+            font-size: 1.6rem;
+            line-height: 1.5;
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1,
+          h2 {
+            font-family: "Roboto", serif;
+          }
+          h1 {
+            font-family: "PT Sans", sans-serif;
+          }
+          ul{
+            list-style:none;
+            margin:0;
+            padding:0;
+          }
+        ` }
+    />
+    <Helmet >
+      <title> Gatsby Hotel </title>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+      <link rel="preconnect"
+        href="https://fonts.googleapis.com" />
+      <link rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet" />
+    </Helmet>
+    <Header />
 
-      {children}
-    </>
+    {children} </>
   )
 }
 
