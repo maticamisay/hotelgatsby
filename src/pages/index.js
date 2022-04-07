337c66fb9e6e2d5385694cf7c7870d912ac10383
maticamisay <maticamisay99@hotmail.com>
2022-04-06 22:56:15 -0300

    creando el componente imagen con graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from "react"
+import ImagenHotel from "../components/imagenHotel"
 import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
     <h1>Gatsby Hotel</h1>
+    <ImagenHotel />
   </Layout>
 )
 
