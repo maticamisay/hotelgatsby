71cd067ebe736a926f302b478f1ad2f25bef2ef3
maticamisay <maticamisay99@hotmail.com>
2022-04-07 23:45:54 -0300

    consultando la pagina de inicio de datocms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from "react"
-import ImagenHotel from "../components/imagenHotel"
 import Layout from "../components/layout"
+import ImagenHotel from "../components/imagenHotel"
+import ContenidoInicio from "../components/contenidoInicio"
+
 
 const IndexPage = () => (
   <Layout>
     <ImagenHotel />
+    <ContenidoInicio />
   </Layout>
 )
 
