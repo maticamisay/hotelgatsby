48e34bd8b8f8402fec06d42653b79a489c587613
maticamisay <maticamisay99@hotmail.com>
2022-04-08 00:15:54 -0300

    css a la informacion de inicio

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import Image from 'gatsby-image'
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const TextoInicio = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media(min-width: 900px){
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        column-gap: 2rem;
+    }
+    p{
+        line-height:2;
+        margin-top: 2rem;
+    }
+`
+
 const ContenidoInicio = () => {
 
-const informacion = useStaticQuery(graphql`
+    const informacion = useStaticQuery(graphql`
 query {
     allDatoCmsPagina(filter: { slug: { eq: "inicio" } }) {
             nodes {
@@ -17,11 +37,22 @@ query {
         }
   }`)
 
-console.log(informacion);
+    const { titulo, contenido, imagen } = informacion.allDatoCmsPagina.nodes[0]
 
     return (
         <>
-            <h2>Titulo de Página</h2>
+            <h2
+            css={css`
+            text-align:center;
+            font-size: 4rem;
+            margin-top: 4rem;
+            `}
+            >{titulo}</h2>
+            <TextoInicio>
+                <p>{contenido}</p>
+                <Image fluid={imagen.fluid} />
+            </TextoInicio>
+
         </>
     );
 }
